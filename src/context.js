ad4eebecb2b58e32d5d51ef68ab57209eee16ae9
Lexa <lexaraea@gmail.com>
2020-04-23 11:54:11 -0400

    Add remove item functionality

--- src/context.js
+++ src/context.js
@@ -77,10 +77,39 @@ const ProductContext = React.createContext()
         console.log("this is decrement")
     };
     removeItem = (id) => {
-        console.log("item removed")
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(
+            () => {
+                return {
+                    cart:[...tempCart],
+                    products:[...tempProducts]
+                };
+            },
+            () => {
+                this.addTotals();
+            } 
+        );
     };
     clearCart = () => {
-        console.log("cart cleared")
+        this.setState(
+            () => {
+                return { cart: [] };
+            }, 
+            () => {
+                this.storeProducts();
+                this.addTotals();
+            }
+        );
     };
     addTotals = ()=> {
         let subTotal = 0;
