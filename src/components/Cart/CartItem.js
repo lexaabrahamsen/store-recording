e553a7c369624f5a806ed7699d54015a803305d1
Lexa <lexaraea@gmail.com>
2020-04-22 21:48:11 -0400

    Add styles

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function CartItem({item,value}) {
-    const{id,title,img,price,total,count} = item;
-    const {increment,decrement,removeItem} = value;
+export default function CartItem({ item,value }) {
+    const{ id,title,img,price,total,count } = item;
+    const { increment,decrement,removeItem } = value;
     return (
        <div className="row my-1 text-capitalize text-center">
            <div className="col-10 mx-auto col-lg-2">
                <img 
                     src={img} 
-                    style={{width:"5rem", height: "5rem"}}
+                    style={{ width:"5rem", height: "5rem" }}
                     className="img-fluid"
                     alt="product"
                 />
@@ -21,7 +21,16 @@ export default function CartItem({item,value}) {
             <span>price :</span>
                 {price}
         </div>
-        
+        <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+        <div>
+            <span className="btn btn-black mx-1" onClick={() => decrement(id)}>-
+
+            </span>
+        </div>
+        </div>
+        </div>
+        {/* {} */}
 
        </div>
     )
