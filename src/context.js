bd75630bb355b18e8ddc9bfccef9e9ca10803f4c
Lexa <lexaraea@gmail.com>
2020-04-23 15:02:35 -0400

    Add decrement functionality

--- src/context.js
+++ src/context.js
@@ -83,7 +83,26 @@ const ProductContext = React.createContext()
         })
     };
     decrement = (id) => {
-        console.log("this is decrement")
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+        product.total = product.count * product.price;
+        if (product.count === 0) {
+            this.removeItem(id)
+        } else {
+            product.total = product.count * product.price;
+            this.setState(
+                () => {
+                    return{ cart:[...tempCart] };
+                }, 
+                () => {
+                    this.addTotals();
+                }
+            );
+        }
     };
     removeItem = (id) => {
         let tempProducts = [...this.state.products];
